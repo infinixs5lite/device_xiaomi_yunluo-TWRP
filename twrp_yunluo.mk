37e73f446a4dd1c7c28420322c9ee4edcbf6169f
Arpitshubhamm <157899697+Arpitshubhamm@users.noreply.github.com>
2024-02-25 15:23:53 +0530

    Update and rename omni_yunluo.mk to twrp_yunluo.mk

--- twrp_yunluo.mk
+++ twrp_yunluo.mk
@@ -9,17 +9,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from yunluo device
 $(call inherit-product, device/xiaomi/yunluo/device.mk)
 
 PRODUCT_DEVICE := yunluo
-PRODUCT_NAME := omni_yunluo
+PRODUCT_NAME := twrp_yunluo
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22081283G
 PRODUCT_MANUFACTURER := xiaomi
+
+# Hide Reflash TWRP
+PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
