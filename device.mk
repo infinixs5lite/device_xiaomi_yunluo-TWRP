f75ac3039dd63544ba90890b47628505844eba71
infinixs5lite <kelvinchinedu97@gmail.com>
2024-02-26 21:50:59 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,6 +13,23 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster41 \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
@@ -20,12 +37,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6789
-
-#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#    bootctrl.mt6789 \
-#    libgptutils \
-#    libz \
-#    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
